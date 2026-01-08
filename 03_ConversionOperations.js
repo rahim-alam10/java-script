441e35f17f2a7dfb5a3b4b970264df70448bcc4c
rahim-alam10 <alamrahim010@gmail.com>
2026-01-08 16:38:33 +0500

    Variables

---
+++ 03_ConversionOperations.js
@@ -0,0 +1,52 @@
+
+/*
+let score = '33'
+
+        // Number Conversion
+console.log(typeof score)
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 333
+// "33abc" =>NaN
+// true =>1 ; false=> 0
+
+
+            // Boolean Conversion
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)  // true
+
+// 1 => true
+//"" =>false
+//"rahim" => true
+
+
+        // String Conversion
+ let someNum=33
+
+ let stringNumber= String(someNum)
+ 
+ console.log(someNum);
+ console.log(typeof stringNumber)
+
+ */
+
+/* 
+
+ console.log("1" + 2)   //12
+ console.log(1 + "2")   //12
+ console.log("1" + 2 + 2)      //122
+ console.log(1 + 2 +"2")       //32 
+
+ console.log(+true)     // 1
+
+*/ 
+
+console.log(null>0)     //false
+console.log(null==0)    //false
+console.log(null>=0)    //true
+
+// comparsions(>,<,>=,<=) converts null into 0 and treat it as a 0 number
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"Use Strict"; //treat all js code as newer version
+// alert(3+3) we are using nodejs, not broswer
+
+
+
+// number => " 2 to the power 53 "
+//bignit
+//string => " "
+//boolean =>true / faalse
+//null=> standalone value
+//undefined =>  
+//symbol => unique
+
+console .log(typeof undefined);     //undefined
+console.log(typeof null)            //object
