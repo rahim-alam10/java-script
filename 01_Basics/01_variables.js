83f78a47512801a548a6d420b2f277cbb9e7c513
rahim-alam10 <rahimalam010@gmail.com>
2026-06-12 17:10:18 +0500

    Basics Level 1

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="alamrahim010@gmail.com"
+var acountpassword="1234"
+acountCity="Islamabad"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, acountpassword, acountCity])
---
+++ 01_Basics/07_Dates.js
@@ -0,0 +1,9 @@
+let mydate= new Date()  //object
+
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+
+let mycreateddate= new Date("2023-01-14")
+
+
